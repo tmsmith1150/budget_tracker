7ef276c7d4394829f3f6e482e33b6db9aea9dead
Callie Crownover <crownovercallie@gmail.com>
2020-08-04 21:41:55 -0400

    worked on hooking bootstrap up to the handlebars file

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,6 +27,14 @@ module.exports = function(app) {
     res.render("signup");
   });
 
+  app.get("/index", (req, res) => {
+    // If the user already has an account send them to the overview page
+    if (req.user) {
+      res.redirect("/overview");
+    }
+    res.render("index");
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/overview", isAuthenticated, (req, res) => {
