10ffa9f3676997b5921ba32b0e1fe29ef46e7cb6
kikaiaku <tjscarb89@aol.com>
2020-08-06 21:05:50 -0400

    updates on db and login fixed

---
+++ models/categories.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    var Categories = sequelize.define("Categorie", {
+      id: {type:DataTypes.INTEGER,primaryKey: true},
+      categoryName: DataTypes.STRING,
+      categoryType: DataTypes.STRING,
+      userId: DataTypes.INTEGER
+    });
+    Categories.associate = function(models) {
+        // Associating Catergories with Expense
+        //
+        Categories.hasMany(models.Expense, {
+          onDelete: "cascade"
+        });
+      };
+
+    return Categories;
+  };
